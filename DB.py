081135a5e4de9cb2a97f14f64b5b6e1082b0eaac
mohsen <mohsen.zolfagharian.98@gmail.com>
2021-03-10 05:17:55 +0330

    create database and update functions

--- DB.py
+++ DB.py
@@ -6,12 +6,15 @@ def create_db(name):
     conn.close()
 
 
-def create_table(table_name, col_name, col_type):
-    conn = sqlite3.connect('WORDS.db')
+def create_table(name_db, table_name, col_name, col_type):
+    conn = sqlite3.connect(f'{name_db}.db')
     cursor = conn.cursor()
     cursor.execute(f""" CREATE TABLE '{table_name}'(
-                        '{col_name}' '{col_type}')""")
+                        '{col_name[0]}' '{col_type[0]}',
+                        '{col_name[1]}' '{col_type[1]}')""")
     conn.commit()
     conn.close()
 
+
+# create_table('WORDS', 'verbs', ['number', 'word'], ['INT', 'TEXT'])
 
