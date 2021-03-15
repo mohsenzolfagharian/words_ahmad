d929a74fc0fb29db6b5712ad10d05a8cb6a965b8
mohsen <mohsen.zolfagharian.98@gmail.com>
2021-03-15 06:38:26 +0330

    add comments to files

--- queries and proccess.py
+++ queries and proccess.py
@@ -1,9 +1,11 @@
 import sqlite3
 
-data_base_col = ['pre', 'verb', 'post']
+# set a list of columns in table
+table_words_col = ['pre', 'verb', 'post']
 
 
 def add_pre(db_name, table_name, word):
+    # just add pre
     conn = sqlite3.connect(f"{db_name}.db")
     cursor = conn.cursor()
     cursor.execute(f"""INSERT INTO '{table_name}'('pre') VALUES('{word}') """)
@@ -12,6 +14,7 @@ def add_pre(db_name, table_name, word):
 
 
 def add_post(db_name, table_name, word):
+    # just add post
     conn = sqlite3.connect(f"{db_name}.db")
     cursor = conn.cursor()
     cursor.execute(f"""INSERT INTO '{table_name}'('post') VALUES('{word}') """)
@@ -20,6 +23,7 @@ def add_post(db_name, table_name, word):
 
 
 def add_verb(db_name, table_name, word):
+    # just add verbs
     conn = sqlite3.connect(f"{db_name}.db")
     cursor = conn.cursor()
     cursor.execute(f"""INSERT INTO '{table_name}'('verb') VALUES('{word}') """)
@@ -28,6 +32,7 @@ def add_verb(db_name, table_name, word):
 
 
 def add_all_info(db_name, table_name, col_name, words):
+    # add data to all columns in a command
     conn = sqlite3.connect(f"{db_name}.db")
     cursor = conn.cursor()
     cursor.execute(f"""INSERT INTO '{table_name}'('{col_name[0]}', '{col_name[1]}','{col_name[2]}') VALUES('{words[0]}',
@@ -55,14 +60,16 @@ def show_info(db_name, table_name, col_name):
 
 # simple search
 def show_all_info(db_name, table_name):
+    # all data in a list with 3 block tuples in it
+    # structure [(post, verb, pre), (post, verb, pre), ..., (post, verb, pre)]
     words_list = []
     conn = sqlite3.connect(f"{db_name}.db")
     cursor = conn.cursor()
-    cursor.execute(f"""SELECT * FROM '{table_name}' """)
+    cursor.execute(f"""SELECT * FROM {table_name} """)
     data = cursor.fetchall()
     for data in data:
         # just showing the word
-        words_list.append(data[0])
+        words_list.append(data)
     conn.commit()
     conn.close()
     return words_list
@@ -96,17 +103,25 @@ def pre_verb_post(pre, verb, post):
     return words
 
 
+# add data with command below
+# add_all_info('final', 'words', table_words_col, ['', '', ''])
+
+# how to call data
 verbs = show_info('final', 'words', 'verb')
 pre = show_info('final', 'words', 'pre')
 post = show_info('final', 'words', 'post')
-
-
+all_data = show_all_info('final', 'words')
+# how to use functions to mix data
 mix = verb_post(verbs, post)
 mix2 = pre_verb(pre, verbs)
 mix3 = pre_verb_post(pre, verbs, post)
+
+# print data alone
 print('verbs :', verbs)
 print('pre :', pre)
 print('post :', post)
+print('all data : ', all_data)
+# print mixed data
 print(mix)
 print(mix2)
 print(mix3)
